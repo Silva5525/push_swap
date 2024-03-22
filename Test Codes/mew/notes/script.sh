8c2cb892158034360da2a9bf57dae239b3af0046
Silva5525 <132822583+Silva5525@users.noreply.github.com>
2024-03-22 14:39:03 +0100

    clean

--- Test Codes/mew/notes/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -d "./libs/mlx" ]
-then
-        echo "Essa biblioteca existe"
-        rm -rf ./libs/mlx
-else
-        echo "Error"
-fi
--- Test Codes/mlx42_first_anim/run.sh
+++
@@ -1,2 +0,0 @@
-make -C libft
-gcc -fsanitize=address -o test image_utils.c animate.c main.c -ldl -lglfw -lm -lpthread -LMLX42/build -lmlx42 -Llibft/ -lft -IMLX42/include -Ilibft/includes && ./test 
