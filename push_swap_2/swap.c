f26ddcc0231c4caaf5fe577c97e98d501cd6eb15
Silva5525 <silvus6@hotmail.com>
2024-04-10 11:39:49 +0200

    now

--- push_swap_2/swap.c
+++ push_swap_2/swap.c
@@ -6,7 +6,7 @@
 /*   By: wdegraf <wdegraf@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 14:23:07 by wdegraf           #+#    #+#             */
-/*   Updated: 2024/04/07 19:18:55 by wdegraf          ###   ########.fr       */
+/*   Updated: 2024/04/07 20:17:06 by wdegraf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 static void	swap(t_link **link)
 {
-	t_link	*a;
-	t_link	*b;
+	t_link	*tmp;
 
-	if (!*link || !(*link)->next || (*link) == (*link)->next)
+	if (!*link || !(*link)->next)
 		return ;
-
+	tmp = (*link)->next;
+	(*link)->next = tmp->next;
+	if ((tmp->next))
+		(tmp->next->prev) = *link;
+	tmp->prev = (*link)->prev;
+	tmp->next = *link;
+	(*link)->prev = tmp;
+	if (tmp->prev)
+		(tmp->prev->next) = tmp;
+	(*link) = tmp;
 }
